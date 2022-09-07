4f711244a82fcbb4ddf8528e0a9ed6de04f316cf
alexeyk500 <alexeyk500@yandex.ru>
2022-09-07 11:11:35 +0300

    css normalize - done

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from './store/store';
 import './index.css';
+import './variabless.css';
 import AppRouter from './components/AppRouter/AppRouter';
 
 const container = document.getElementById('root')!;
