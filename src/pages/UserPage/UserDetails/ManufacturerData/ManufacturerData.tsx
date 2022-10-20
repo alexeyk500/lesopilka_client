4bd6c43502872ccd691d0feb24c6a3930b613359
alexeyk500 <alexeyk500@yandex.ru>
2022-10-20 16:10:52 +0300

    UserPage - Done

--- src/pages/UserPage/UserDetails/ManufacturerData/ManufacturerData.tsx
+++ src/pages/UserPage/UserDetails/ManufacturerData/ManufacturerData.tsx
@@ -17,12 +17,28 @@ const ManufacturerData: React.FC = () => {
         <div className={classes.rowContainer}>
           <div className={classes.dataContainer}>
             <div className={classes.rowDataContainer}>
+              <div className={classes.title}>{'Название :'}</div>
+              <div className={classes.value}>{user?.manufacturer?.title}</div>
+            </div>
+            <div className={classes.rowDataContainer}>
               <div className={classes.title}>{'ИНН :'}</div>
               <div className={classes.value}>{user?.manufacturer?.inn}</div>
             </div>
             <div className={classes.rowDataContainer}>
-              <div className={classes.title}>{'Название :'}</div>
-              <div className={classes.value}>{user?.manufacturer?.title}</div>
+              <div className={classes.title}>{'Регион :'}</div>
+              <div className={classes.value}>{user?.manufacturer?.address.region.title}</div>
+            </div>
+            <div className={classes.rowDataContainer}>
+              <div className={classes.title}>{'Населенный пункт :'}</div>
+              <div className={classes.value}>{user?.manufacturer?.address.location.title}</div>
+            </div>
+            <div className={classes.rowDataContainer}>
+              <div className={classes.title}>{'Адрес :'}</div>
+              <div className={classes.value}>
+                {`${user?.manufacturer?.address.street}, ${user?.manufacturer?.address.building} ${
+                  user?.manufacturer?.address.office ? ' ,' : ''
+                }${user?.manufacturer?.address.office ? user?.manufacturer?.address.office : ''}`}
+              </div>
             </div>
             <div className={classes.rowDataContainer}>
               <div className={classes.title}>{'Телефон :'}</div>
