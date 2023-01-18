b1075c3a3c32385058bf07415acbf9bec771cadb
alexeyk500 <alexeyk500@yandex.ru>
2023-01-18 18:10:07 +0300

    css styles - fixed

--- src/pages/OrdersPage/OrdersPageControl/DateIntervalSelector/DateIntervalSelector.tsx
+++ src/pages/OrdersPage/OrdersPageControl/DateIntervalSelector/DateIntervalSelector.tsx
@@ -1,12 +1,8 @@
-import React from "react";
-import classes from './DateIntervalSelector.module.css'
+import React from 'react';
+import classes from './DateIntervalSelector.module.css';
 
 const DateIntervalSelector = () => {
-  return (
-    <div className={classes.container}>
-      DateIntervalSelector
-    </div>
-  );
+  return <div className={classes.container}>DateIntervalSelector</div>;
 };
 
 export default DateIntervalSelector;
--- src/pages/OrdersPage/OrdersPageControl/OrdersPageControl.tsx
+++ src/pages/OrdersPage/OrdersPageControl/OrdersPageControl.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import DateIntervalSelector from "./DateIntervalSelector/DateIntervalSelector";
-import StatusSelector from "./StatusSelector/StatusSelector";
-import classes from './OrdersPageControl.module.css'
+import DateIntervalSelector from './DateIntervalSelector/DateIntervalSelector';
+import StatusSelector from './StatusSelector/StatusSelector';
+import classes from './OrdersPageControl.module.css';
 
 const OrdersPageControl: React.FC = () => {
-
   return (
     <>
       <div className={classes.containerStatusSelector}>
