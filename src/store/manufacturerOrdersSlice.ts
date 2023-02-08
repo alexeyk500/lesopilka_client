1468211a758727c2361ddd2757ba7e9d4b0d1793
alexeyk500 <alexeyk500@yandex.ru>
2023-02-08 11:23:06 +0300

    Return to previous

--- src/store/manufacturerOrdersSlice.ts
+++
@@ -1,9 +0,0 @@
-import {OrderType} from "../types/types";
-
-type ManufacturerOrdersSliceType = {
-  selectedOrderStatusId: number;
-  dateFrom: string;
-  dateTo: string;
-  orders: OrderType[];
-  isLoading: boolean;
-};
