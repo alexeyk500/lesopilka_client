cf9bc822b62756d6abaf69b0831b1dcc867f1059
alexeyk500 <alexeyk500@yandex.ru>
2023-04-03 13:28:58 +0300

    Bug with price list - fixed

--- src/api/priceApi.ts
+++ src/api/priceApi.ts
@@ -2,11 +2,7 @@ import axios from 'axios';
 
 export const priceApi = {
   async getPricePDF(mid: number) {
-    // const response = await axios.get(`${process.env.REACT_APP_APP_BASE_URL_API}/price/${mid}`, {
-    //   responseType: 'stream',
-    // });
-    const response = await axios(`${process.env.REACT_APP_APP_BASE_URL_API}/price/${mid}`, {
-      method: 'GET',
+    const response = await axios.get(`${process.env.REACT_APP_APP_BASE_URL_API}/price/${mid}`, {
       responseType: 'blob',
     });
     return response.data;
